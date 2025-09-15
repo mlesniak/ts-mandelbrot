8cff65ac20f8ea52372616b6a92e773d69ce4900
Michael Lesniak <mail@mlesniak.com>
2025-09-15 08:28:18 +0200

    Refactoring

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import fs from "fs";
 
-const width = 300;
-const height = 200;
+// @mlesniak cli with fancy progress bar or so...
+
+const width = 100;
+const height = 73;
 
 let boundaries = {
     minX: -2,
@@ -13,6 +15,39 @@ let boundaries = {
 let stepX = (boundaries.maxX - boundaries.minX) / width;
 let stepY = (boundaries.maxY - boundaries.minY) / height;
 let maxIterations = 255;
+
+abstract class Image {
+    protected pixel: boolean[];
+    readonly width: number;
+    readonly height: number;
+
+    constructor(width: number, height: number) {
+        this.width = width;
+        this.height = height;
+        this.pixel = Array(width * height);
+        this.pixel.fill(false);
+    }
+
+    set(x: number, y: number) {
+        this.pixel[width * y + x] = true;
+    }
+
+    abstract write(filename: string): void;
+}
+
+class PPMImage extends Image {
+    write(filename: string) {
+        let image = fs.openSync(filename, "w");
+        fs.writeSync(image, `P1\n${width} ${height}\n`);
+
+        let pixels = this.pixel
+            .map(b => b === undefined ? 0 : b ? 1 : 0)
+        let p = pixels.join(" ");
+        fs.writeSync(image, p);
+        fs.writeSync(image, "\n");
+        fs.closeSync(image);
+    }
+}
 
 class Point {
     readonly x: number
@@ -35,35 +70,29 @@ class Point {
             iteration++;
         }
 
-        return !(iteration === maxIterations);
+        return iteration === maxIterations;
     }
 }
 
-let image = fs.openSync("output.ppm", "w");
-fs.writeSync(image, `P1
-${width} ${height}
-`);
+let ppmImage: Image = new PPMImage(width, height);
 
 let start = Date.now();
 for (let y = 0; y < height; y++) {
     console.log(y);
     let yt = y * stepY + boundaries.minY;
-    let row = []
 
     for (let x = 0; x < width; x++) {
         let xt = x * stepX + boundaries.minX;
-
         let point = new Point(xt, yt);
-        let mb = point.isInMandelbrotSet() ? 1 : 0;
-        row.push(mb);
+        if (point.isInMandelbrotSet()) {
+            ppmImage.set(x, y);
+        }
     }
-
-    let fileRow = row.join(" ") + "\n";
-    fs.writeSync(image, fileRow);
 }
+
+ppmImage.write("output.ppm")
+
 let end = Date.now();
 let diff = end - start;
 console.log(diff / 1000.0);
-console.log(width * height / Math.pow(10,6));
-
-fs.closeSync(image);
+console.log(width * height / Math.pow(10, 6));
