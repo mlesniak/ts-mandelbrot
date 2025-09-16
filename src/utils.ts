be34d87d3f392a7387b3f35e38d60e1c82de8ab7
Michael Lesniak <mail@mlesniak.com>
2025-09-16 11:05:20 +0200

    Dynamically show timing information

--- src/utils.ts
+++ src/utils.ts
@@ -1,14 +1,6 @@
-// export function timer<R>(f: (...args: any[]) => R): R {
-//     let start = Date.now();
-//     const result = f();
-//     let end = Date.now();
-//     let diff = end - start;
-//     console.log(diff / 1000.0);
-//     return result;
-// }
-
-
 export function timed(label?: string): MethodDecorator {
+    const showTiming = process.env.TIMING ?? false;
+
     return (_target, key, descriptor: PropertyDescriptor) => {
         const func = descriptor.value as (...args: any[]) => any;
         descriptor.value = function(...args: any[]) {
@@ -17,8 +9,9 @@ export function timed(label?: string): MethodDecorator {
             const end = Date.now();
             const diff = end - start;
 
-            // @mlesniak only show if environment variable is set.
-            console.log(`${label ?? String(key)}: ${diff / 1000.0}`)
+            if (showTiming) {
+                console.log(`${label ?? String(key)}: ${diff / 1000.0}`);
+            }
             return result;
         }
         return descriptor;
